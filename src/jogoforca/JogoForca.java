e33f841d1ba30973a378c885b9918e7b908be203
yuri.jwsilva <yuri.jwsilva@CAS555596W0750.senacsp.edu.br>
2017-05-08 22:18:14 -0300

    Adicionando mecanica ao jogo

--- src/jogoforca/JogoForca.java
+++ src/jogoforca/JogoForca.java
@@ -13,27 +13,89 @@ import java.util.Scanner;
  */
 public class JogoForca {
     public static void main(String[] args) {
-        String palavra = "Queijo";
-        String letrasUtilizadas = "";
+        String palavra = "Macarrao";
+        String letrasUtilizadas = "", letrasDescartadas = "";
         desenhaHumano(0);
-        desenhaCampo(palavra);
-        digitarLetra();     
+        desenhaCampo(palavra, "");
+        int tentativas = 0;
+        while (true) {   
+            char letra = digitarLetra(letrasUtilizadas, letrasDescartadas);
+
+            boolean possuiLetra = false;
+
+            for (int i = 0; i < palavra.length(); i++) {
+                if(letra == palavra.toUpperCase().charAt(i)) {
+                    letrasUtilizadas += letra;
+                    desenhaCampo(palavra, letrasUtilizadas);
+                    possuiLetra = true;
+                    break;
+                }
+            }
+            if(!possuiLetra) {
+                letrasDescartadas += letra;
+                desenhaHumano(0);
+            }
+            if(ganhou(palavra, letrasUtilizadas)) {
+                System.out.println("Você ganhou! Parabéns!");
+                break;
+            } else if(tentativas >= 6) {
+                System.out.println("Você perdeu! Tente novamente!");
+                break;
+            }
+        }
     }
     
-    public static void desenhaCampo (String palavra) {
+    public static void desenhaCampo (String palavraInicial, String letrasValidadas) {
         String campo = "";
-        for (int i = 0; i < palavra.length(); i++) {
-            campo += " _ ";
+        palavraInicial = palavraInicial.toUpperCase();
+        
+        for (int i = 0; i < palavraInicial.length(); i++) {
+            boolean validado = false;
+            for (int j = 0; j < letrasValidadas.length(); j++) {
+                if(palavraInicial.charAt(i) == letrasValidadas.charAt(j)) {
+                    campo += " " + letrasValidadas.charAt(j) + " ";
+                    validado = true;
+                    break;
+                }
+            }
+            if(!validado)
+                campo += " _ ";
         }
         System.out.println("Palavra: " + campo);
     }
     
-    public static char digitarLetra() {
+    public static char digitarLetra (String letrasUtilizadas, String letrasDescartadas) {
         Scanner console = new Scanner(System.in);
+        char letra;
+        letrasDescartadas = letrasDescartadas.toUpperCase();
+        letrasUtilizadas = letrasUtilizadas.toUpperCase();
         
-        System.out.println("Digite uma letra:");
-        char letra = 'A';
-        return 'A';
+        while (true) {            
+            System.out.println("Digite uma letra: ");
+            letra = console.nextLine().toUpperCase().charAt(0);
+            
+            boolean letraUsada = false;
+            for (int i = 0; i < letrasUtilizadas.length(); i++) {
+                if(letra == letrasUtilizadas.charAt(i)) {
+                    System.out.println("A letra " + letra + " já foi utilizada. Tente novamente.");
+                    letraUsada = true;
+                    break;
+                }
+            }
+            if(!letraUsada) {
+                for (int i = 0; i < letrasDescartadas.length(); i++) {
+                    if(letra == letrasDescartadas.charAt(i)) {
+                        System.out.println("A letra " + letra + " já foi utilizada. Tente novamente.");
+                        letraUsada = true;
+                        break;
+                    }
+                }
+            }
+            
+            if(!letraUsada)
+                break;
+        }
+        return letra;
     }
     
     public static void desenhaHumano(int nivel) {
@@ -49,5 +111,19 @@ public class JogoForca {
                 break;
         }
         System.out.println(desenho);
+    }
+  
+    public static boolean ganhou (String palavra, String letrasUtilizadas) {
+        int letrasConfirmadas = 0;
+        palavra = palavra.toUpperCase();
+        for (int i = 0; i < palavra.length(); i++) {
+            for (int j = 0; j < letrasUtilizadas.length(); j++) {
+                if(palavra.charAt(i) == letrasUtilizadas.charAt(j))
+                    letrasConfirmadas++;
+            }
+        }
+        if(letrasConfirmadas >= palavra.length())
+            return true;
+        return false;
     }
 }
