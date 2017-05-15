d04763410e1762ae5be9c628c17f6c804c3c8321
roberto.jwsilva <roberto.jwsilva@CAS555595W0751.senacsp.edu.br>
2017-05-15 19:23:20 -0300

    JogoForca

--- src/jogoforca/JogoForca.java
+++ src/jogoforca/JogoForca.java
@@ -14,11 +14,37 @@ import java.util.Scanner;
  */
 public class JogoForca {
     public static void main(String[] args) {
-        
-        String letrasUtilizadas = "";
+        String palavra = sorteiaPalavra();
+        String letrasUtilizadas = "", letrasDescartadas = "";
         desenhaHumano(0);
-        desenhaCampo(sorteiaPalavra());
-        digitarLetra();
+        desenhaCampo(palavra, "");
+        int tentativas = 0;
+        while (true) {   
+            char letra = digitarLetra(letrasUtilizadas, letrasDescartadas);
+        
+            boolean possuiLetra = false;
+
+            for (int i = 0; i < palavra.length(); i++) {
+                if(letra == palavra.toUpperCase().charAt(i)) {
+                    letrasUtilizadas += letra;
+                    desenhaCampo(palavra, letrasUtilizadas);
+                    possuiLetra = true;
+                    break;
+    }
+            }
+            if(!possuiLetra) {
+                letrasDescartadas += letra;
+                tentativas++;
+                desenhaHumano(tentativas);
+            }
+            if(ganhou(palavra, letrasUtilizadas)) {
+                System.out.println("Você ganhou! Parabéns!");
+                break;
+            } else if(tentativas >= 6) {
+                System.out.println("Você perdeu! Tente novamente!");
+                break;
+            }
+        }
     }
     
     public static String sorteiaPalavra (){
@@ -32,11 +58,11 @@ public class JogoForca {
         palavras[1] = "fabrica";
         palavras[2] = "algoritimo";
         palavras[3] = "computador";
-        palavras[4] = "lógica";
-        palavras[5] = "programação";
+        palavras[4] = "logica";
+        palavras[5] = "programacao";
         palavras[6] = "bacon";
         palavras[7] = "senac";
-        palavras[8] = "são paulo";
+        palavras[8] = "sao paulo";
         palavras[9] = "tecnologia";
         
         String palavra = palavras[nPalavra];
@@ -45,20 +71,57 @@ public class JogoForca {
     
     
     
-    public static void desenhaCampo (String palavra) {
+    public static void desenhaCampo (String palavraInicial, String letrasValidadas) {
         String campo = "";
-        for (int i = 0; i < palavra.length(); i++) {
+        palavraInicial = palavraInicial.toUpperCase();
+        
+        for (int i = 0; i < palavraInicial.length(); i++) {
+            boolean validado = false;
+            for (int j = 0; j < letrasValidadas.length(); j++) {
+                if(palavraInicial.charAt(i) == letrasValidadas.charAt(j)) {
+                    campo += " " + letrasValidadas.charAt(j) + " ";
+                    validado = true;
+                    break;
+                }
+            }
+            if(!validado)
             campo += " _ ";
         }
         System.out.println("Palavra: " + campo);
     }
     
-    public static char digitarLetra() {
+    public static char digitarLetra (String letrasUtilizadas, String letrasDescartadas) {
         Scanner console = new Scanner(System.in);
+        char letra;
+        letrasDescartadas = letrasDescartadas.toUpperCase();
+        letrasUtilizadas = letrasUtilizadas.toUpperCase();
         
-        System.out.println("Digite uma letra:");
-        char letra = 'A';
-        return 'A';
+        while (true) {            
+            System.out.println("Digite uma letra: ");
+            letra = console.nextLine().toUpperCase().charAt(0);
+            
+            boolean letraUsada = false;
+            for (int i = 0; i < letrasUtilizadas.length(); i++) {
+                if(letra == letrasUtilizadas.charAt(i)) {
+                    System.out.println("A letra " + letra + " já foi utilizada. Tente novamente.");
+                    letraUsada = true;
+                    break;
+    }
+            }
+            if(!letraUsada) {
+                for (int i = 0; i < letrasDescartadas.length(); i++) {
+                    if(letra == letrasDescartadas.charAt(i)) {
+                        System.out.println("A letra " + letra + " já foi utilizada. Tente novamente.");
+                        letraUsada = true;
+                        break;
+                    }
+                }
+            }
+    
+            if(!letraUsada)
+                break;
+        }
+        return letra;
     }
     
     public static void desenhaHumano(int nivel) {
@@ -189,10 +252,10 @@ public class JogoForca {
                 desenho += " ||=========||\n";
                 desenho += " ||     .-\"\"\"\"\"\"\"-. \n";
                 desenho += " || /^\\/  _.   _.  \\/^\\\n";
-                desenho += " || \\(   /\\_/\\ /\\_/\\   )/\n";
-                desenho += " ||  \\,  \\/_\\/_\\/_\\/  ,/\n";
+                desenho += " || \\(    X    X     )/\n";
+                desenho += " ||  \\,            ,/\n";
                 desenho += " ||    \\    (_)    /\n";
-                desenho += " ||     `-.'==='.-' \n";
+                desenho += " ||     `-.,===,.-'\n";
                 desenho += " ||      __) - (__\n";
                 desenho += " ||     /  `~~~`  \\ \n";
                 desenho += " ||    / //     \\\\ \\ \n";
@@ -207,5 +270,19 @@ public class JogoForca {
                 break;
         }
         System.out.println(desenho);
+    }
+  
+    public static boolean ganhou (String palavra, String letrasUtilizadas) {
+        int letrasConfirmadas = 0;
+        palavra = palavra.toUpperCase();
+        for (int i = 0; i < palavra.length(); i++) {
+            for (int j = 0; j < letrasUtilizadas.length(); j++) {
+                if(palavra.charAt(i) == letrasUtilizadas.charAt(j))
+                    letrasConfirmadas++;
+}
+        }
+        if(letrasConfirmadas >= palavra.length())
+            return true;
+        return false;
     }
 }
