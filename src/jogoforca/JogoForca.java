c19e07b4d3cc236dfda35e03a1f4877da65c75c2
roberto.jwsilva <roberto.jwsilva@CAS555595W0751.senacsp.edu.br>
2017-05-25 19:50:19 -0300

    novo

--- src/jogoforca/JogoForca.java
+++ src/jogoforca/JogoForca.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jogoForca;
+package jogoforca;
 
 import java.util.Random;
 import java.util.Scanner;
